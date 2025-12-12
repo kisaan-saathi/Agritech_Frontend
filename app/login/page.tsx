597a5c753ff82012e24f0033c80684cf0a971dce
TrushaKyada <trusha@genxai.com>
2025-12-12 10:55:38 +0530

    integreate login and sign up api

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,27 +2,48 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-
-
+import { useState } from "react";
+import axios from "axios";
 export default function Login() {
   const router = useRouter();
-
-  const handleLogin = (e: any) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const handleLogin = async (e: any) => {
+    setLoading(true);
     e.preventDefault();
-    router.push("/dashboard");
+    try {
+      const res = await axios.post("http://localhost:4000/api/v1/auth/login", {
+        email,
+        password,
+      });
+      if (res.data.statusCode == 200) {
+        localStorage.setItem("refreshToken", res.data.data.refreshToken);
+        localStorage.setItem("accessToken", res.data.data.accessToken);
+        alert(res.data.message);
+        router.push("/dashboard");
+      } else {
+        alert(res.data.message);
+      }
+    } catch (err: any) {
+      console.log("Login error", err);
+      alert(`${err.message}`);
+    } finally {
+      setEmail("");
+      setPassword("");
+      setLoading(false);
+    }
   };
 
   return (
     <div className="login-body">
       <div className="container-fluid h-100">
         <div className="row no-gutter h-100">
-          
           {/* LEFT SIDE */}
           <div className="col-md-6 bg-light">
             <div className="login d-flex align-items-center h-100">
               <div className="container">
                 <div className="row">
-
                   <div className="text-center pb-3">
                     <Image
                       src="/images/GenXAILatest.png"
@@ -44,6 +65,7 @@ export default function Login() {
                           className="form-control"
                           placeholder="email"
                           required
+                          onChange={(e) => setEmail(e.target.value)}
                         />
                         <label>Email Address</label>
                       </div>
@@ -54,6 +76,7 @@ export default function Login() {
                           className="form-control"
                           placeholder="password"
                           required
+                          onChange={(e) => setPassword(e.target.value)}
                         />
                         <label>Password</label>
                       </div>
@@ -63,13 +86,24 @@ export default function Login() {
                           className="btn btn-success text-uppercase rounded shadow-sm"
                           type="submit"
                         >
-                          Login
+                          {loading ? (
+                            <div
+                              className="spinner-border text-light"
+                              role="status"
+                            >
+                              <span className="sr-only">Loading...</span>
+                            </div>
+                          ) : (
+                            "Login"
+                          )}
                         </button>
 
-                          <a href="/signup" className="btn btn-outline-primary text-uppercase rounded shadow-sm">
-    Sign Up
-  </a>
-
+                        <a
+                          href="/signup"
+                          className="btn btn-outline-primary text-uppercase rounded shadow-sm"
+                        >
+                          Sign Up
+                        </a>
 
                         <a href="#" className="text-decoration-none">
                           Forgot password?
@@ -77,7 +111,6 @@ export default function Login() {
                       </div>
                     </form>
                   </div>
-
                 </div>
               </div>
             </div>
@@ -85,7 +118,6 @@ export default function Login() {
 
           {/* RIGHT SIDE IMAGE */}
           <div className="col-md-6 d-none d-md-flex bg-image"></div>
-
         </div>
       </div>
 
